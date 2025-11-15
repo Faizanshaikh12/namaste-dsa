7fdfd5ea3093b062ce756a92b4b7d2a47df289c1
Faizan <faizanroy000777@gmail.com>
2025-11-15 13:38:24 +0530

    Add dry run table for removeDuplicates function in removeDupplicates.js

--- removeDupplicates.js
+++ removeDupplicates.js
@@ -24,3 +24,22 @@ const k = removeDuplicates(nums);
 console.log("Input:", nums);
 console.log("Length after removing duplicates:", k);
 console.log("Array after removing duplicates:", nums.slice(0, k));
+
+
+/* 
+Dry Run Table
+i | nums[i] | x | nums[x] | Condition (nums[i] > nums[x]) | Action Taken         | Updated x | Updated nums[x]
+-------------------------------------------------------------------------------------------
+0 |    0    | 0 |    0    |            false              | No action            |     0     |      0
+1 |    0    | 0 |    0    |            false              | No action            |     0     |      0
+2 |    1    | 0 |    0    |            true               | x = x + 1; nums[x] = nums[i] |     1     |      1
+3 |    1    | 1 |    1    |            false              | No action            |     1     |      1
+4 |    1    | 1 |    1    |            false              | No action            |     1     |      1
+5 |    2    | 1 |    1    |            true               | x = x + 1; nums[x] = nums[i] |     2     |      2
+6 |    2    | 2 |    2    |            false              | No action            |     2     |      2
+7 |    3    | 2 |    2    |            true               | x = x + 1; nums[x] = nums[i] |     3     |      3
+8 |    3    | 3 |    3    |            false              | No action            |     3     |      3
+9 |    4    | 3 |    3    |            true               | x = x + 1; nums[x] = nums[i] |     4     |      4
+-------------------------------------------------------------------------------------------
+Final Value of x: 4
+*/
